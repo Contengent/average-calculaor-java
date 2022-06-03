110a1dd3bc3daec40871774ac17217946db6a60e
Contengent <103345406+Contengent@users.noreply.github.com>
2022-06-03 00:15:52 -0400

    Add files via upload

---
+++ averageCalculator/src/main/java/com/mycompany/averagecalculator/AverageCalculator.java
@@ -0,0 +1,17 @@
+package com.mycompany.averagecalculator;
+
+/**
+ * @author 
+ *
+ *   /\_/\  _     ___
+ *  |>'v'<||"| -/\, ,`>
+ *     -=Edelstein=-
+ * 
+ */
+
+public class AverageCalculator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
